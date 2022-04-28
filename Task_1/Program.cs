07c584759eaab81a94604f85f9bb6e0c98fc0428
liza <lizashatuzhilkina@gmail.com>
2022-04-28 15:22:21 +0300

    Добавила минимальное значение

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -6,11 +6,20 @@ int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число 2 -> ");
 int num2 = int.Parse(Console.ReadLine());
 if (num1 > num2)
-{
+{   Console.Write("Максимальное число, ");
     Console.Write(num1);
 }
 else
 {
-     Console.Write(num2);
+    Console.Write("Максимальное число, ");
+    Console.Write(num2);
 }
-
+if (num2 < num1)
+{   Console.WriteLine("Минимальное число, ");
+    Console.Write(num2);
+}
+else
+{
+    Console.Write("Минимальное число, ");
+    Console.Write(num1);
+}
